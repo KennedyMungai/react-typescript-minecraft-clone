4a21bcaceb2e135f4980443f334fc3f246ee499e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 12:08:19 +0300

    Created the movement state

--- src/hooks/useKeyboard.tsx
+++ src/hooks/useKeyboard.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
 const useKeyboard = () =>
 {
-
+    const [movement, setMovement] = useState({
+        moveForward: false,
+        moveBackward: false,
+        moveLeft: false,
+        moveRight: false,
+        jump: false,
+        texture1: false,
+        texture2: false,
+        texture3: false,
+        texture4: false,
+        texture5: false,
+    })
 }
 
 export default useKeyboard
