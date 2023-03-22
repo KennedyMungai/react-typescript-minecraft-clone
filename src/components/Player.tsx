177d711f3378ce438aa268840b821276e2fc88ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 11:42:38 +0300

    Added a console log to ensure the useFrame code is running

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -18,6 +18,7 @@ const Player = () =>
 
     useFrame(() => 
     {
+        console.log('frame')
         camera.position.copy(new Vector3(position.current[0], position.current[1], position.current[2]))
     })
 
