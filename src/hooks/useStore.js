115f55bab41c4f648315025d089776841deb35df
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 14:55:51 +0300

    Partially wrote the useStore arrow function

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -2,8 +2,21 @@ import { create } from "zustand";
 
 
 const useStore = create((set) =>
-{
-
-})
+({
+    texture: 'dirt',
+    cubes: [],
+    addCube: (x, y, z) =>
+    {
+        set((prev) => (
+            {
+                cubes: [...prevCubes, { key: }]
+            }
+        ))
+    },
+    removeCube: () => { },
+    setTexture: () => { },
+    saveWorld: () => { },
+    resetWorld: () => { },
+}))
 
 export default useStore
