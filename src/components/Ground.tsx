a36cd9a66290454b14d9457d70eac1534dbfc37f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 10:51:18 +0300

    Returned a mesh with  geometry and material

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,13 +1,27 @@
 import { usePlane } from '@react-three/cannon'
 import React from 'react'
+import { Mesh } from 'three'
 
 
 const Ground = () =>
 {
-    const [ref] = usePlane(() =>
+    const [ref] = usePlane<Mesh>(() =>
     ({
         rotation: [0, 0, 0], position: [0, 0, 0]
     }))
+
+    return (
+        <mesh ref={ref}>
+            <planeBufferGeometry
+                attach='geometry'
+                args={[100, 100]}
+            />
+            <meshStandardMaterial
+                attach='material'
+                color='hotpink'
+            />
+        </mesh>
+    )
 }
 
 export default Ground
