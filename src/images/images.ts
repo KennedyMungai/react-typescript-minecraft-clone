8675ba161fa8fa7fdc51bbfd8965507aab77079a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 10:14:02 +0300

    Created and exported the images object

--- src/images/images.ts
+++ src/images/images.ts
@@ -1,12 +1,13 @@
-import React from 'react'
+import dirtImg from './dirt.jpg'
+import grassImg from './grass.jpg'
+import glassImg from './glass.png'
+import logImg from './log.jpg'
+import woodImg from './wood.png'
 
-type Props = {}
-
-const images = (props: Props) =>
-{
-    return (
-        <div>images</div>
-    )
+export const images = {
+    dirtImg,
+    grassImg,
+    glassImg,
+    logImg,
+    woodImg
 }
-
-export default images
