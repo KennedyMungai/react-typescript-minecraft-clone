ae260e8de7cbed5ed62b8332db8563e5a030dae5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 11:35:20 +0300

    Created a useSphere mesh

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,17 @@
 import { useSphere } from "@react-three/cannon"
 import { useThree } from "@react-three/fiber"
+import { Mesh } from "three"
 
 
 const Player = () =>
 {
     const { camera } = useThree()
+    const [ref] = useSphere<Mesh>(() =>
+    ({
+        mass: 1,
+        type: 'Dynamic',
+        position: [0, 0, 0]
+    }))
 }
 
 export default Player       
