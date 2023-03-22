7a2448888f216be03abdd601aca25501194bd491
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 10:18:33 +0300

    Converted images to textures

--- src/images/textures.ts
+++ src/images/textures.ts
@@ -1,2 +1,8 @@
 import { TextureLoader } from "three";
 import { dirtImg, glassImg, grassImg, logImg, woodImg } from "./images";
+
+const dirtTexture = new TextureLoader(dirtImg)
+const glassTexture = new TextureLoader(glassImg)
+const grassTexture = new TextureLoader(grassImg)
+const logTexture = new TextureLoader(logImg)
+const woodTexture = new TextureLoader(woodImg)
