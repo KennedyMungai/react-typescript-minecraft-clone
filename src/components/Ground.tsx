4b0d5ebab78021a9658317b4c4b5be5b4e7c7278
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 11:57:42 +0300

    Rotated the mesh to 90 degrees

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -7,7 +7,7 @@ const Ground = () =>
 {
     const [ref] = usePlane<Mesh>(() =>
     ({
-        rotation: [-Math.PI / 4, 0, 0], position: [0, 0, 0]
+        rotation: [-Math.PI / 2, 0, 0], position: [0, 0, 0]
     }))
 
     groundTexture.magFilter = NearestFilter
