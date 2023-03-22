33944963ca23ad87005048a92fe33d220beb63d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 11:38:19 +0300

    Created the position ref

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,6 @@
 import { useSphere } from "@react-three/cannon"
 import { useThree } from "@react-three/fiber"
+import { useRef } from "react"
 import { Mesh } from "three"
 
 
@@ -12,6 +13,8 @@ const Player = () =>
         type: 'Dynamic',
         position: [0, 0, 0]
     }))
+
+    const position = useRef<[int, int, int]>([0, 0, 0])
 
     return (
         <mesh ref={ref}>
