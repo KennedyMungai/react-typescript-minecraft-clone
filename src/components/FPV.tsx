8ca032a4569ed8dcf4b51c19d71e53f1c6895d4c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 14:35:23 +0300

    Returned the PointerLockControls component with an array of destructured camera and gl passed in an array

--- src/components/FPV.tsx
+++ src/components/FPV.tsx
@@ -1,8 +1,15 @@
+import { PointerLockControls } from "@react-three/drei"
 import { useThree } from "@react-three/fiber"
 
 const FPV = () =>
 {
-    const {camera} = useThree()
+    const { camera, gl } = useThree()
+
+    return (
+        <PointerLockControls
+            args={[camera, gl.domElement]}
+        />
+    )
 }
 
 export default FPV
