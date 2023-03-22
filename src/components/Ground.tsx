b8b1a662022bd189c506dbe4115a848b87c1b801
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 11:02:25 +0300

    Made the texture crisp

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,5 +1,5 @@
 import { usePlane } from '@react-three/cannon'
-import { Mesh, RepeatWrapping } from 'three'
+import { Mesh, NearestFilter, RepeatWrapping } from 'three'
 import { groundTexture } from '../images/textures'
 
 
@@ -10,6 +10,7 @@ const Ground = () =>
         rotation: [0, 0, 0], position: [0, 0, 0]
     }))
 
+    groundTexture.magFilter = NearestFilter
     groundTexture.wrapS = RepeatWrapping
     groundTexture.wrapT = RepeatWrapping
     groundTexture.repeat.set(100, 100)
