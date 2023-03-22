1b265611457818d39b8b41024e4ce9656bcf86ee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-22 15:17:45 +0300

    Instantiated the useBox hook

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -3,7 +3,11 @@ import { useBox } from "@react-three/cannon"
 
 const Cube = ({ position, texture }) =>
 {
-
+    const [ref] = useBox(() => 
+    ({
+        type: 'Static',
+        position
+    }))
 }
 
 export default Cube
