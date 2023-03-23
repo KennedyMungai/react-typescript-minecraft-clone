b4a81fe7d02a3d5fce93f3e84ed9e4f77d20347a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 07:43:26 +0300

    Added a reference of the active texture from useStore

--- src/components/TextureSelector.tsx
+++ src/components/TextureSelector.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import useStore from "../hooks/useStore"
 
 const TextureSelector = () =>
 {
     const [visible, setVisible] = useState<boolean>(false)
+    const [activeTexture] = useStore((state) => [state.texture])
 }
 
 export default TextureSelector
