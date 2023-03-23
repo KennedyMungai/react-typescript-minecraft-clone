a6a517b1c236a8f1ca9109ae275c0176c40dcdcf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 08:33:22 +0300

    Added access for the saveWorld and resetWorld functions from the store

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ import useStore from "../hooks/useStore"
 
 const Menu = () => 
 {
-    const [saveWorld, resetWorld] = useStore()
+    const [saveWorld, resetWorld] = useStore((state) => [state.saveWorld, state.resetWorld])
 }
 
 export default Menu
