559b54605b3f571b1850d004b26b619c4fe2b5bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 07:36:17 +0300

    Used the position element of the cube

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -27,7 +27,7 @@ const useStore = create((set) =>
             {
                 cubes: prev.cubes.filter(cube => 
                 {
-                    const [X, Y, Z] = cube
+                    const [X, Y, Z] = cube.position
                     return X != x && Y != y && Z != z
                 })
             }
