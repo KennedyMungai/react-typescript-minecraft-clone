6784f0c00f94e72ced87d937af1266be5cf58378
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 07:39:53 +0300

    Created the TextureSelector react component

---
+++ src/components/TextureSelector.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const TextureSelector = (props: Props) =>
+{
+    return (
+        <div>TextureSelector</div>
+    )
+}
+
+export default TextureSelector
