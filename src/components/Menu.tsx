4337df5c394dc590f320d5b7fee5b025538a97e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-23 08:38:23 +0300

    Rendered out some markup for the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,6 +4,13 @@ import useStore from "../hooks/useStore"
 const Menu = () => 
 {
     const [saveWorld, resetWorld] = useStore((state) => [state.saveWorld, state.resetWorld])
+
+    return (
+        <div className="menu absolute">
+            <button>Save</button>
+            <button>Reset</button>
+        </div>
+    )
 }
 
 export default Menu
